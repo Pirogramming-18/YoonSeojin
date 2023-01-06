997e25d2ed77a8c9f2b604518772f903ac2795cf
se0jinYoon <tjwls1999@gmail.com>
2023-01-07 00:21:39 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,43 +1,37 @@
-num = 0
-gamePlay = True
+def brGame() :
+    num = 0
+    gamePlay = True
+    turn = 0
+    attacker = None
+    defender = None
 
-while gamePlay:
-    #PlayerA
-    while True:
-        try:
-            playN = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-            if playN == 1 or playN == 2 or playN == 3:
+    while gamePlay:
+        if turn % 2 == 0:
+            attacker = 'playerA'
+            defender = 'playerB'
+        else:
+            attacker = 'playerB'
+            defender = 'playerA'
+        turn += 1
+        
+        while True:
+            try:
+                playN = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                if playN == 1 or playN == 2 or playN == 3:
+                    break
+                else:
+                    print('1,2,3 중 하나를 입력하세요')
+                    continue
+            except ValueError as e:
+                print('정수를 입력하세요')
+
+        for i in range(num+1, num+playN+1): 
+            print('{} : {}'.format(attacker, i))
+            num += 1 
+            if num == 31:
+                gamePlay = False
+                print('{} win!'.format(defender))
                 break
-            else:
-                print('1,2,3 중 하나를 입력하세요')
-                continue
-        except ValueError as e:
-            print('정수를 입력하세요')
 
-    for i in range(num+1, num+playN+1): 
-        print('playerA : {}'.format(i))
-        num += 1 
-        if num == 31:
-            gamePlay = False
-            print('palyerB win!')
-            break
-
-    #playerB
-    while True:
-        try:
-            playN = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-            if playN == 1 or playN == 2 or playN == 3:
-                break
-            else:
-                print('1,2,3 중 하나를 입력하세요')
-                continue
-        except ValueError as e:
-            print('정수를 입력하세요')
-
-    for i in range(num+1, num+playN+1) : 
-        print('playerB : {}'.format(i))
-        num += 1
-        if num == 31:
-            gamePlay = False
-            print(print('palyerA win!'))
-            break
+if __name__ == "__main__":
+    brGame()
