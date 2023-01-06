d170747ae955cdcad31233f3da4231f032101b39
se0jinYoon <tjwls1999@gmail.com>
2023-01-06 22:30:22 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,12 @@
 num = 0
 
-playN = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+while True:
+    try:
+        playN = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        if playN == 1 or playN == 2 or playN == 3:
+            break
+        else:
+            print('1,2,3 중 하나를 입력하세요')
+            continue
+    except ValueError as e:
+        print('정수를 입력하세요')
