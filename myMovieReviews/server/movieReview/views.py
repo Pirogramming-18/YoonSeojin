3f2f555efeaf3a2898d552a674657c8ed435c896
se0jinYoon <tjwls1999@gmail.com>
2023-01-13 00:21:14 +0900

    기능1 완성

--- myMovieReviews/server/movieReview/views.py
+++ myMovieReviews/server/movieReview/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import MovieReview
 
 def movie_list(request) :
-    return render(request, 'movieReview/movie_list.html', {})
+    movies = MovieReview.objects.all()
+    text = request.GET.get("text")
+    if text:
+        movies = movies.filter(content__contains=text)
+    return render(request, 'movieReview/movie_list.html', {"movies": movies})
